1da7a00ce50291138b8948fb05397ac53faeed8e
Abi-097 <120549516+Abi-097@users.noreply.github.com>
2024-04-20 00:41:36 +0530

    data changed

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -270,7 +270,7 @@ const Hero = () => {
                 <Button
                   variant="outlined"
                   startIcon={<GitHubIcon style={{ fontSize: 30 }} />}
-                  href={Bio.resume}
+                  href={Bio.github}
                   target="_blank"
                   sx={{
                     background:
@@ -302,7 +302,7 @@ const Hero = () => {
                 <Button
                   variant="outlined"
                   endIcon={<DownloadRoundedIcon style={{ fontSize: 30 }} />}
-                  href={Bio.github}
+                  href={Bio.resume}
                   target="_blank"
                   sx={{
                     background:
